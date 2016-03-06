bba81ee27e341e7fcbc4d2b84b914525d383ef34
ermontross <emma.montross@colorado.edu>
2016-03-05 22:34:38 -0700

    playing with urls and extensions

---
+++ background.js
@@ -0,0 +1,20 @@
+//background.js
+//csci 3308 project - party time for those offline
+//https://robots.thoughtbot.com/how-to-make-a-chrome-extension
+
+//called when the user clicks on the browser action (icon in chrome browser)
+chrome.broswerAction.onClicked.addListener(function(tab) {
+  //send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, funciton(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_broswer_action"});
+  });
+});
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if(request.message === "open_new_tab") {
+      chrome.tabs.create({"url": request.url});
+    }
+  }
+);
