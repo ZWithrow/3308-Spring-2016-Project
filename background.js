0225fcbca6e0f6c28dccd9958f167a6a49913779
ermontross <emma.montross@colorado.edu>
2016-03-13 18:22:07 -0600

    fixed function spelling

--- background.js
+++ background.js
@@ -5,7 +5,7 @@
 //called when the user clicks on the browser action (icon in chrome browser)
 chrome.broswerAction.onClicked.addListener(function(tab) {
   /*send a message to the active tab*/
-  chrome.tabs.query({active: true, currentWindow: true}, funciton(tabs) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_broswer_action"});
   });
