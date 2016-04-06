23b67c638ff761770b7fdd354319fb6c2a09a482
Interfeller <michael.j.feller@colorado.edu>
2016-04-06 17:06:34 -0600

    Add connection tester

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function center(id) {
 	else
 		l.style.visibility == 'visible';
 }
+
+if (navigator.onLine) {
+        window.alert("online");
+} else {
+        window.alert("offline");
+}
+
+
+window.addEventListener("offline", function(e) {window.alert("offline"); });
+window.addEventListener("online", function(e) {window.alert("online"); });
