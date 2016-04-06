bda87593a1b4e8e04f88d5a5c00f8e973f7d0ef2
Zach Withrow <Zach Withrow>
2016-04-06 16:15:58 -0600

    adding laser collisions again

--- AsteroidGame.js
+++ AsteroidGame.js
@@ -15,9 +15,14 @@ var laserX = shipPlacementX + 50;
 var laserY = shipPlacementY + 28;
 var Lasers = [];
 var LaserCounter = 0;
+var asteroids = [];
+var asteroidCounter = 0;
 var timer = 0;
+var timer2 = 0;
+var Score = 0;
 for (b = 0; b <= 99; b++) {
-    Lasers[b] = { x: laserX, y: laserY };
+    Lasers[b] = { x: laserX, y: laserY, hit: false };
+    asteroids[b] = { x: 600, y: 600, dx: 1 * Math.floor((Math.random() * 4) + 1), hit: false, Xsize: Math.random() * 20 + 10, Ysize: Math.random() * 20 + 10 };
 }
 
 document.addEventListener("keydown", keyDownHandler, false);
@@ -85,54 +90,113 @@ function fireLaser() {
 function showLasers() {
     var i = 0;
 
-    while (i < LaserCounter) {
-        var laserpx = Lasers[i].x + 2;
-        var laserpy = Lasers[i].y;
-        Lasers[i].x = laserpx;
-        ctx.beginPath();
-        ctx.rect(laserpx, laserpy, 8, 5);
-        ctx.fillStyle = "#00FF00";
-        ctx.fill();
-        ctx.closePath();
+    while (i < 100) {
+        if (Lasers[i].hit == false) {
+            var laserpx = Lasers[i].x + 2;
+            var laserpy = Lasers[i].y;
+            Lasers[i].x = laserpx;
+            ctx.beginPath();
+            ctx.rect(laserpx, laserpy, 8, 5);
+            ctx.fillStyle = "#00FF00";
+            ctx.fill();
+            ctx.closePath();
+        }
+
         i++;
     }
 
 }
-function asteroids() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = "#696969";
-    ctx.fill();
-    ctx.closePath();
+function makeAsteroid() {
+    if (asteroidCounter == 100) {
+        asteroidCounter = 0;
+    }
+    var lx = 500;
+    var ly = Math.floor((Math.random() * 40) + 1) * 12;
+    asteroids[asteroidCounter].x = lx;
+    asteroids[asteroidCounter].y = ly;
+    asteroidCounter++;
+    timer2++;
+}
+function showAsteroids() {
+    var i = 0;
+
+    while (i < asteroidCounter) {
+        if (asteroids[i].hit == false) {
+            var asteroidpx = asteroids[i].x - asteroids[i].dx;
+            asteroids[i].x = asteroidpx;
+            ctx.beginPath();
+            //ctx.arc(asteroidpx, asteroids[i].y, ballRadius, 0, Math.PI * 2);
+            ctx.rect(asteroidpx, asteroids[i].y, asteroids[i].Xsize, asteroids[i].Xsize);
+            ctx.fillStyle = "#696969";
+            ctx.fill();
+            ctx.closePath();
+
+        }
+        i++;
+    }
+}
+
+function collisionDetection() {
+    for (a = 0; a < 100; a++) {
+        for (l = 0; l < 100; l++) {
+            var Laser = Lasers[l];
+            var Aster = asteroids[a];
+            if (Laser.hit == false && Aster.hit == false) {
+                if (Laser.y + 5 > Aster.y && Laser.y < Aster.y + Aster.Xsize && Laser.x + 8 > Aster.x && Laser.x < Aster.x + Aster.Xsize) {
+                    Laser.hit = true;
+                    Aster.hit = true;
+                    Score += 10;
+                }
+            }
+
+        }
+    }
+}
+function updateScore() {
+    ctx.font = "30px Arial";
+    ctx.fillText("Score : " + Score, 25, 25);
 }
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    //if (spaceStop == false) {
-    //    spaceBar();
-    //}
+    if (spaceStop == false) {
+        spaceBar();
+    }
     laserY = shipPlacementY + 28;
     if (timer == 25) {
         timer = 0;
+    }
+    if (timer2 == 50) {
+        timer2 = 0;
     }
     drawShip();
     if (timer == 0) {
         fireLaser();
     }
+
     if (timer != 0) {
         timer++;
     }
 
+
     if (LaserCounter != 0) {
         showLasers();
     }
+    if (timer2 == 0) {
+        makeAsteroid();
+    }
+    timer2++;
+    if (asteroidCounter != 0) {
+        showAsteroids();
+    }
 
-
+    collisionDetection();
     if (downPressed && shipPlacementY < canvas.height - shipHeight) {
         shipPlacementY += 3;
     }
     else if (upPressed && shipPlacementY > 0) {
         shipPlacementY -= 3;
     }
+    updateScore();
     laserX += 1;
 }
 var timer1 = setInterval(draw, 10);
