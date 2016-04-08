f1fef98bdac25b6221537d82c9bce9219e16d056
wrightMatthew <wright.matthew91@gmail.com>
2016-04-08 12:07:52 -0600

    Changed some functionality within partytimepopup, both the html and the javascript files.

--- partyTimePopUp.js
+++ partyTimePopUp.js
@@ -1,9 +1,10 @@
-if (navigator.onLine) {
-        window.alert("online");
-} else {
-        window.alert("offline");
-}
+// if (navigator.onLine) {
+//         window.alert("online");
+// }
+// else {
+//         window.alert("offline");
+// }
 
 
-window.addEventListener("offline", function(e) {window.alert("offline"); });
-window.addEventListener("online", function(e) {window.alert("online"); });
+// window.addEventListener("offline", function(e) {window.alert("offline"); });
+// window.addEventListener("online", function(e) {window.alert("online"); });
