ee019b63279b537540a19650fabda0c647b3d274
Zach Withrow <Zach Withrow>
2016-04-10 11:40:58 -0600

    added scoreboard, starting text and game over text, and added velocity change of ball in the x direction based on paddle position

--- TestGame.js
+++ TestGame.js
@@ -16,10 +16,13 @@ var brickColumnCount = 5;
 var brickWidth = 75;
 var brickHeight = 20;
 var brickPadding = 10;
-var brickOffsetTop = 30;
+var brickOffsetTop = 60;
 var brickOffsetLeft = 30;
 var bricks = [];
 var space = false;
+var Score = 0;
+var START = true;
+var paddleAcceleration = 0;
 for (c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
     for (r = 0; r < brickRowCount; r++) {
@@ -46,6 +49,11 @@ function drawPaddle() {
     ctx.fill();
     ctx.closePath();
 
+}
+function updateScore() {
+    ctx.font = "30px Impact";
+    ctx.fillStyle = "black";
+    ctx.fillText("Score : " + Score, 30, 40);
 }
 function keyDownHandler(e) {
     if (e.keyCode == 39) {
@@ -93,27 +101,67 @@ function collisionDetection() {
                 if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
                     dy = -dy;
                     b.status = 0;
+                    Score += 10;
                 }
             }
         }
     }
 }
+function AllBricksGone() {
+    for (c = 0; c < brickColumnCount; c++) {
+        for (r = 0; r < brickRowCount; r++) {
+            var b = bricks[c][r];
+            if (b.status == 1) {
+                    return
+                }
+            }
+        }
+    for (A = 0; A < brickColumnCount; A++) {
+        for (B = 0; B < brickRowCount; B++) {
+            bricks[A][B].status = 1;
+            
+            }
+        }
+    }
+    
+
 function spaceBar() {
     if (space == false) {
         setTimeout(spaceBar, 50);
         clearInterval(timer1)
         return;
     }
+    START = false;
     timer1 = setInterval(draw, 10);
+    
 
+}
+function finalWait() {
+    if (space == false) {
+        setTimeout(finalWait, 50);
+        clearInterval(timer1)
+        return;
+    }
+    timer1 = setInterval(GameOver, 10);
+}
+function GameOver() {
+    document.location.reload();
 }
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    if (START == true) {
+        ctx.font = "55px Impact";
+        ctx.fillText("Start Game", 480 / 2 - 125, 480 / 2);
+        ctx.font = "20px Impact";
+        ctx.fillText("by pressing SPACEBAR", 480 / 2 - 90, 480 / 2 + 30);
+    }
     if (space == false) {
         spaceBar();
     }
+    updateScore();
     drawBall();
     drawPaddle();
+    AllBricksGone();
     drawBricks();
     collisionDetection();
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
@@ -125,22 +173,37 @@ function draw() {
     else if (y + dy > canvas.height - ballRadius) {
         if (x > paddleX && x < paddleX + paddleWidth) {
             dy = -(dy)-0.5;
+            if (paddleX + (paddleWidth / 2) >= x) {
+                dx -= (x / (paddleX + (paddleWidth / 2))) * 0.5;
+            }
+            else {
+                dx += ((x - 2(x - (paddleX + (paddleWidth / 2)))) / (paddleX + (paddleWidth / 2))) * 0.5;
+            }
+            paddleAcceleration += 0.25;
+            
         }
         else {
-            //alert("Game Over");
+            ctx.font = "55px Impact";
+            ctx.fillStyle = "black";
+            ctx.fillText("GAME OVER", 480 / 2 - 125, 480 / 2);
+            ctx.font = "20px Impact";
+            ctx.fillText("by pressing SPACEBAR", 480 / 2 - 90, 480 / 2 + 30);
             space = false;
-            document.location.reload();
+            if (space == false) {
+                finalWait();
+            }
         }
         
     }
 
     if (rightPressed && paddleX < canvas.width - paddleWidth) {
-        paddleX += 5;
+        paddleX += 5 + paddleAcceleration;
     }
     else if (leftPressed && paddleX > 0) {
-        paddleX -= 5;
+        paddleX -= 5 + paddleAcceleration;
     }
     x += dx;
     y += dy;
+    
 }
 var timer1 = setInterval(draw, 10);
