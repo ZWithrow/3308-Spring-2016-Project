cafc4e24eeab4ca9d5cf6e96509106c5dee063be
wrightMatthew <wright.matthew91@gmail.com>
2016-04-11 19:10:11 -0600

    Moved partytime to css folder, deleted useless partyTimePopUp html

--- partyTimePopUp.js
+++ partyTimePopUp.js
@@ -5,6 +5,18 @@
 //         window.alert("offline");
 // }
 
+if (navigator.onLine) {
+	document.getElementById(network).innerHTML = "network status: online"
+} else {
+	document.getElementById(network).innerHTML = "network status: offline"
+}
+
+if(navigator.online){
+	document.write(nline yo.)
+}
+
+document.write(5 + 6);
+
 
 window.addEventListener("offline", function(e) {window.alert("offline"); });
 window.addEventListener("online", function(e) {window.alert("online"); });
