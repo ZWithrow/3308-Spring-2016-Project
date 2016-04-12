17266c02d291ebcffef1151e61371714e26a700e
ermontross <emma.montross@colorado.edu>
2016-04-11 19:56:05 -0600

    messing with online/offline status

--- partyTimePopUp.js
+++ partyTimePopUp.js
@@ -1,3 +1,18 @@
+
+// window.addEventListener('statusCheck', function (e) {
+// 	if (navigator.onLine) {
+// 		document.getElementById('online').style.display = 'block';
+// 		document.getElementById('offline').style.display = 'none';
+// 	} else {
+// 		document.getElementById('offline').style.display = 'block';
+// 		document.getElementById('online').style.display = 'none';
+// 	}
+// });
+
+
+/*window.addEventListener("offline", status);
+window.addEventListener("online", status);*/
+
 if (navigator.onLine) {
 	document.getElementById('online').style.display = 'block';
 	document.getElementById('offline').style.display = 'none';
@@ -5,7 +20,3 @@ if (navigator.onLine) {
 	document.getElementById('offline').style.display = 'block';
 	document.getElementById('online').style.display = 'none';
 }
-
-
-window.addEventListener("offline", function(e) {window.alert("offline"); });
-window.addEventListener("online", function(e) {window.alert("online"); });
