19d22b5d8fe9075cf6500ff4d9b8848cf4c3092b
ermontross <emma.montross@colorado.edu>
2016-04-17 19:44:23 -0600

    internet status working

--- partyTimePopUp.js
+++ partyTimePopUp.js
@@ -1,53 +1,5 @@
-
-// window.addEventListener('statusCheck', function (e) {
-// 	if (navigator.onLine) {
-// 		document.getElementById('online').style.display = 'block';
-// 		document.getElementById('offline').style.display = 'none';
-// 	} else {
-// 		document.getElementById('offline').style.display = 'block';
-// 		document.getElementById('online').style.display = 'none';
-// 	}
-// });
-
-
-// window.addEventListener("online", function(e) {
-// 	onvar = document.getElementById('online');
-// 	offvar = document.getElementById('offline');
-// 	if(onvar.style.display == 'none'){
-// 		onvar.style.display = 'block';
-// 		offvar.style.display = 'none';
-// 	}
-// 	else{
-// 		onvar.style.display = 'none';
-// 		offvar.style.display = 'block';
-// 	}
-// 	/*document.getElementById("online").style.display = 'block';
-// 	document.getElementById("offline").style.display = 'none';*/
-// });
-// window.addEventListener("offline", function(e) {
-// 	document.getElementById("offline").style.display = 'block';
-// 	document.getElementById("online").style.display = 'none';
-// });
-
-// window.addEventListener("online", status);
-// window.addEventListener("offline", status);
-
-// function status(e) {
-// 	if (navigator.onLine) {
-// 		document.getElementById('online').style.display = 'block';
-// 		document.getElementById('offline').style.display = 'none';
-// 	} else {
-// 		document.getElementById('offline').style.display = 'block';
-// 		document.getElementById('online').style.display = 'none';
-// 	}
-// }
-
-// window.addEventListener("offline", function(e) {window.alert("offline"); });
-// window.addEventListener("online", function(e) {window.alert("online"); });
-
-
 if (navigator.onLine) {
-        window.alert("online");
+        document.getElementById("status").innerHTML = "<p>Internet Status: Online</p>";
 } else {
-        window.alert("offline");
+        document.getElementById("status").innerHTML = "<p>Internet Status: Offline</p>";
 }
