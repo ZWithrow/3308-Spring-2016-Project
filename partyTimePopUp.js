ac977587e8992444725372050123ee049f14e296
wrightMatthew <wright.matthew91@gmail.com>
2016-04-23 22:12:46 -0600

    Gawd, that popup is pretty. Well relatively, its like me, you know, a solid 6, 7 on a good day. Its a smaller popup than others, but it does what it needs to do. Pop ups are all about how you use them, you know? Oh, but I added two images I made for buttons, made them links, resized some stuff, and just general other stuff all within the partytime popup stuff. Another commit is coming, but it'll be just commenting my code and whatnot. Cheers. Oh, and the button says Aow. I hope thats cool

--- partyTimePopUp.js
+++ partyTimePopUp.js
@@ -1,7 +1,11 @@
 /** Detects whether the internet is online or offline
 * and displays the status on the extension popup. */
 if (navigator.onLine) {
-        document.getElementById("status").innerHTML = "<p>Internet Status: Online</p>";
+        // document.getElementById("status").innerHTML = "<p>Internet Status: Online</p>";  WORKING CODE
+		// document.getElementById("status").innerHTML = "<a class="internetContainer" href = "http://www.speedtest.net/" target="_blank" > To test you're internet speeds via speedtest.net, click here.. </a>";
+		// document.getElementById('status').innerHTML = "<a href='http://www.speedtest.net/'><img src='images/onlineImage.png' height=50px width=260px</a>";
+		document.getElementById('status').innerHTML = "<a href='index.html' target='_blank'><img src='images/onlineImage.png' height=50px width=260px</a>";
+
 } else {
-        document.getElementById("status").innerHTML = "<p>Internet Status: Offline</p>";
+        document.getElementById("status").innerHTML = "<a href='index.html' target='_blank'><img src='images/offlineImage.png' height=50px width=260px</a>";
 }
