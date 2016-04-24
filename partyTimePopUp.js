eadf6f9f80b458abc6badc398d34505b81727ca9
wrightMatthew <wright.matthew91@gmail.com>
2016-04-23 22:16:40 -0600

    The commit I promised, a smaller message.

--- partyTimePopUp.js
+++ partyTimePopUp.js
@@ -1,11 +1,9 @@
 /** Detects whether the internet is online or offline
 * and displays the status on the extension popup. */
 if (navigator.onLine) {
-        // document.getElementById("status").innerHTML = "<p>Internet Status: Online</p>";  WORKING CODE
-		// document.getElementById("status").innerHTML = "<a class="internetContainer" href = "http://www.speedtest.net/" target="_blank" > To test you're internet speeds via speedtest.net, click here.. </a>";
-		// document.getElementById('status').innerHTML = "<a href='http://www.speedtest.net/'><img src='images/onlineImage.png' height=50px width=260px</a>";
-		document.getElementById('status').innerHTML = "<a href='index.html' target='_blank'><img src='images/onlineImage.png' height=50px width=260px</a>";
+	// If the internet is online, the below code shows an online green button and links to a speed test. Otherwise it just says the internet is out, and give a red button
+	document.getElementById('status').innerHTML = "<a href='index.html' target='_blank'><img src='images/onlineImage.png' height=50px width=260px</a>";
 
 } else {
-        document.getElementById("status").innerHTML = "<a href='index.html' target='_blank'><img src='images/offlineImage.png' height=50px width=260px</a>";
+	document.getElementById("status").innerHTML = "<a href='index.html' target='_blank'><img src='images/offlineImage.png' height=50px width=260px</a>";
 }
